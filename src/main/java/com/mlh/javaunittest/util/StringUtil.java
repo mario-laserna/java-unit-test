4d943d878fa854c5efcc3b824fce2af0707a3435
Mario <laserna.mario@gmail.com>
2021-03-16 18:35:55 -0500

    test para argumentos ilegales

--- src/main/java/com/mlh/javaunittest/util/StringUtil.java
+++ src/main/java/com/mlh/javaunittest/util/StringUtil.java
@@ -3,6 +3,10 @@ package com.mlh.javaunittest.util;
 public class StringUtil {
 
     public static String repeat(String str, int times){
+        if(times < 0){
+            throw new IllegalArgumentException("negative times is not allowed");
+        }
+
         String result = "";
 
         for (int i = 0; i < times; i++) {
