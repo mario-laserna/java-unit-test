3e9d5b9ad19ac7317b042679fb03f7be5ae5a908
Mario <laserna.mario@gmail.com>
2021-07-15 18:56:02 -0500

    ejercicio de tests para funcion de isEmpty en StringUtil

--- src/test/java/com/mlh/javaunittest/util/StringUtilTest.java
+++ src/test/java/com/mlh/javaunittest/util/StringUtilTest.java
@@ -25,4 +25,23 @@ public class StringUtilTest {
         StringUtil.repeat("hola", -3);
     }
 
+    @Test
+    public void str_is_not_empty(){
+        Assert.assertFalse(StringUtil.isEmpty("Mario"));
+    }
+
+    @Test
+    public void str_is_empty(){
+        Assert.assertTrue(StringUtil.isEmpty(""));
+    }
+
+    @Test
+    public void str_null_is_empty(){
+        Assert.assertTrue(StringUtil.isEmpty(null));
+    }
+
+    @Test
+    public void str_spaces_is_empty(){
+        Assert.assertTrue(StringUtil.isEmpty("  "));
+    }
 }
