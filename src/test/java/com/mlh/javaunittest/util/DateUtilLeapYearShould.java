41ca4dd5f4b2eae8aad4d7f5018c647b0b44696e
Mario <laserna.mario@gmail.com>
2021-07-17 06:15:19 -0500

    cambio de assertThat que esta deprecated por assertTrue y assertFalse según el caso

--- src/test/java/com/mlh/javaunittest/util/DateUtilLeapYearShould.java
+++ src/test/java/com/mlh/javaunittest/util/DateUtilLeapYearShould.java
@@ -10,35 +10,35 @@ public class DateUtilLeapYearShould {
     /*
         All years divisible by 400 ARE Leap years (1600, 2000, 2400)
         All years divisible by 100 but not by 400 are NOT leap years (1700, 1800, 1900),
-        All years divisible by 4 but not by 100 ARE 1eap years (1996, 2004, 2008),
+        All years divisible by 4 but not by 100 ARE leap years (1996, 2004, 2008),
         All years not divisible by 4 are NOT Leap years (2017, 2018, 2019)
      */
 
     @Test
     public void return_true_when_is_divisible_by_400() {
-        assertThat(DateUtil.isLeapYear(1600), is(true));
-        assertThat(DateUtil.isLeapYear(2000), is(true));
-        assertThat(DateUtil.isLeapYear(2400), is(true));
+        assertTrue(DateUtil.isLeapYear(1600));
+        assertTrue(DateUtil.isLeapYear(2000));
+        assertTrue(DateUtil.isLeapYear(2400));
     }
 
     @Test
     public void return_false_when_is_divisible_by_100_but_not_by_400() {
-        assertThat(DateUtil.isLeapYear(1700), is(false));
-        assertThat(DateUtil.isLeapYear(1800), is(false));
-        assertThat(DateUtil.isLeapYear(1900), is(false));
+        assertFalse(DateUtil.isLeapYear(1700));
+        assertFalse(DateUtil.isLeapYear(1800));
+        assertFalse(DateUtil.isLeapYear(1900));
     }
 
     @Test
     public void return_true_when_year_is_divisible_by_4_but_not_by_100() {
-        assertThat(DateUtil.isLeapYear(1996), is(true));
-        assertThat(DateUtil.isLeapYear(2004), is(true));
-        assertThat(DateUtil.isLeapYear(2008), is(true));
+        assertTrue(DateUtil.isLeapYear(1996));
+        assertTrue(DateUtil.isLeapYear(2004));
+        assertTrue(DateUtil.isLeapYear(2008));
     }
 
     @Test
     public void return_false_when_is_not_divisible_by_4() {
-        assertThat(DateUtil.isLeapYear(2017), is(false));
-        assertThat(DateUtil.isLeapYear(2018), is(false));
-        assertThat(DateUtil.isLeapYear(2019), is(false));
+        assertFalse(DateUtil.isLeapYear(2017));
+        assertFalse(DateUtil.isLeapYear(2018));
+        assertFalse(DateUtil.isLeapYear(2019));
     }
 }
